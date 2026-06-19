740e214b631b3f51ba6e26c118d6e016dc664196
Pratik Kumar <pratikkumarsbg@gmail.com>
2026-06-19 23:56:23 +0530

    showing data from db

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from '@/components/Hero';
 import HomeProperties from '@/components/HomeProperties';
 import Infoboxes from '@/components/Infoboxes';
+import connectDB from '@/config/database';
 
 export default function Home() {
+  connectDB();
   return (
     <>
       <Hero />
--- components/HomeProperties.tsx
+++ components/HomeProperties.tsx
@@ -1,29 +1,38 @@
-import Link from 'next/link';
-import PropertyCard from './PropertyCard';
-import properties from '@/properties.json';
+import Link from "next/link";
+import PropertyCard from "./PropertyCard";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+// import properties from '@/properties.json';
 
-const HomeProperties = () => {
-  const recentProperties = properties.slice(0, 3);
+const HomeProperties = async () => {
+  await connectDB();
+  const recentProperties = await Property.find({})
+    .sort({ createdAt: -1 })
+    .limit(3)
+    .lean();
   //
   return (
     <>
-      <section className='px-4 py-6'>
-        <div className='container-xl lg:container m-auto px-4 py-6'>
-          <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>Recent Properties</h2>
-          {recentProperties.length === 0 ?
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+            Recent Properties
+          </h2>
+          {recentProperties.length === 0 ? (
             <p>No properties found.</p>
-          : <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
               {recentProperties.map((property) => (
                 <PropertyCard key={property._id} property={property} />
               ))}
             </div>
-          }
+          )}
         </div>
       </section>
-      <section className='m-auto max-w-lg my-10 px-6'>
+      <section className="m-auto max-w-lg my-10 px-6">
         <Link
-          href='/properties'
-          className='block w-full bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700background'
+          href="/properties"
+          className="block w-full bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700background"
         >
           View All Properties
         </Link>
